379ff6acec23f5b58a3dda63b2e5951ff719c959
Joe Jamsky <cba1067950@yahoo.com>
2019-04-25 21:36:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -10,6 +10,17 @@ def using_pop(inArray)
   inArray.pop()
 end
 
-def pop_with_args(inArray, itemNum)
-  inArray.pop(itemNum)
+def pop_with_args(inArray, popCount)
+  inArray.pop(popCount)
 end
+
+def using_shift(inArray)
+  inArray.shift()
+end
+
+def shift_with_args(inArray, shiftCount)
+  inArray.shift(shiftCount)
+end
+
+def using_concat(array1, array2)
+  array
